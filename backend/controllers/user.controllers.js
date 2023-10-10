8a740af5a9392ef4e2ee309b9b740704d0522ef5
Pranay <pmcanvas4501@gmail.com>
2023-10-10 22:56:10 +0530

    orders logic for backend is added and orders component is created for frontend

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,15 +1,24 @@
 var jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
 const { UsersModel } = require('../model/user.model');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
+    const avatar = req.file;
+    const { name, email, password } = req.body;
 
     if (!email || !name || !password) {
-        return res.status(400).send({ "msg": "Fill all the field!" })
+        return res.status(400).send({ "msg": "Fill all the fields!" });
     } else {
-        let newUser = new UsersModel({ name, email, password })
-        await newUser.save()
-        return res.status(201).send({ "msg": "User created successfully!" })
+        let newUser = new UsersModel({ name, email, password });
+        newUser.avatar = `${avatar.filename}`;
+
+        try {
+            await newUser.save();
+            return res.status(201).send({ "msg": "User created successfully!" });
+        } catch (error) {
+            return res.status(500).send({ "msg": "Error creating user" });
+        }
     }
 }
 
@@ -26,7 +35,10 @@ const login = async (req, res) => {
         } else {
             if (password == user.password) {
                 var token = jwt.sign({ email: email, name: user.name }, 'shhhhh');
-                return res.status(200).send({ token: token, msg: "login successful!", name: user.name })
+                const avatarPath = path.join(__dirname, '../uploads', user.avatar);
+                const imageData = fs.readFileSync(avatarPath);
+                const base64Image = imageData.toString('base64');
+                return res.status(200).send({ token: token, msg: "login successful!", name: user.name, avatar: base64Image || null })
             } else {
                 return res.status(400).send({ "msg": "wrong password" })
             }
@@ -39,4 +51,48 @@ const verify = (req, res) => {
     res.status(200).send({ "msg": "valid user", name: name })
 }
 
-module.exports = { register, login, verify }
+
+const placeOrder = async (req, res) => {
+    const { email, orders } = req.body;
+
+    try {
+        let user = await UsersModel.findOne({ email });
+
+        if (user) {
+            orders.forEach((ele) => {
+                user.orders.push(ele);
+            });
+
+            await user.save();
+
+            return res.status(201).send("Orders saved successfully");
+        } else {
+            return res.status(404).send("User not found");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send("Orders couldn't be saved");
+    }
+};
+
+
+
+const getOrder = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        let user = await UsersModel.findOne({ email }).populate('orders.product');
+
+        if (user) {
+            return res.status(201).send(user);
+        } else {
+            return res.status(404).send("User not found");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send("Orders couldn't be fetched");
+    }
+};
+
+
+module.exports = { register, login, verify, placeOrder, getOrder }
--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
+    avatar: { type: String },
     name: {
         type: String, required: [true, 'Name is required']
     },
@@ -9,7 +10,14 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String, required: [true, "Password field can't be empty"]
-    }
+    },
+    orders: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            quantity: { type: Number, default: 1 },
+            date: { type: Date, default: Date.now }
+        }
+    ]
 })
 
 const UsersModel = mongoose.model('User', userSchema)
