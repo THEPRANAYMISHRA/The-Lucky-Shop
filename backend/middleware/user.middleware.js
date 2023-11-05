fdc1269ec22319e22bd2e244ac0e406312f3ab99
Pranay <pmcanvas4501@gmail.com>
2023-11-05 09:14:41 +0530

    logout feature is added

--- backend/middleware/user.middleware.js
+++ backend/middleware/user.middleware.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const { blacklistModel } = require('../model/blacklist.model');
 
-const middleware = (req, res, next) => {
+const middleware = async (req, res, next) => {
     const token = req.headers.authorization;
 
     if (!token) {
-        return res.status(401).json({ error: "Please provide a valid token" });
+        return res.status(401).json({ error: "Please provide a token" });
+    } else {
+        let isThisTokenIsBlackListed = await blacklistModel.findOne({ token })
+        if (isThisTokenIsBlackListed) {
+            return res.status(401).json({ error: "Token already expired" });
+        } else {
+            const decoded = jwt.verify(token, process.env.jwtkey);
+            if (decoded) {
+                req.body.name = decoded.name
+                req.body.email = decoded.email
+                next();
+            } else {
+                return res.status(400).json({ msg: "Unauthorized user!" });
+            }
+        }
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.jwtkey);
-        if (decoded) {
-            req.body.name = decoded.name
-            req.body.email = decoded.email
-            next();
-        } else {
-            return res.status(400).json({ msg: "Unauthorized user!" });
-        }
-    } catch (error) {
-        return res.status(401).json({ error: "Invalid token" });
-    }
+
 };
 
 module.exports = { middleware };
